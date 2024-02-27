f70f1a8c2df2ee9671ecd67c68f8f6aa7496fab9
Soumyadipta Kar <k.sammy2002@gmail.com>
2024-02-27 20:35:18 +0530

    requirements added

--- template.py
+++ template.py
@@ -21,7 +21,8 @@ list_of_files = [
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trails.ipynb"
+    "research/trails.ipynb",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
